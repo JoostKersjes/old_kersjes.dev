541e0fec7f9b5bf5de9e755128d2e636cff77b9b
Joost Kersjes <joost.kersjes@live.nl>
2021-02-22 19:09:30 +0100

    Move pages to src/pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 import '../styles/tailwind.css';
 
 function KersjesDevApp({ Component, pageProps }: AppProps): ReactNode {
-  return <Component {...pageProps} />;
+  return <><Component {...pageProps} /></>;
 }
 
 export default KersjesDevApp;
