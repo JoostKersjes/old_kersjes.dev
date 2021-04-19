e4e8e64b68e4c30ca07ae94e83170014c825063a
Joost Kersjes <joost.kersjes@live.nl>
2021-04-19 18:51:30 +0200

    Upgrade package versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,7 @@ module.exports = {
         'plugin:react/recommended', // React rules
         'plugin:react-hooks/recommended', // React hooks rules
         'plugin:jsx-a11y/recommended', // Accessibility rules
-        'prettier/@typescript-eslint', // Prettier plugin
-        'plugin:prettier/recommended', // Prettier recommended rules
+        'plugin:prettier/recommended', // Prettier plugin
       ],
       rules: {
         // Use TypeScript's types for component props instead
